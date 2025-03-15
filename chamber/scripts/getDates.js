a282de95cf2c17a57dccf2d2280c2257eec48550
carcent <carli.centurion@gmail.com>
2025-03-15 13:50:30 -0300

    start of chamber project

---
+++ chamber/scripts/getDates.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+
+    var lastModifiedDate = document.lastModified;
+
+    document.getElementById('lastModified').textContent = lastModifiedDate;
+
+    var lastYear = new Date(document.lastModified);
+
+    var year = lastYear.getFullYear();
+
+    document.getElementById('lastYear').textContent = year;
+};
